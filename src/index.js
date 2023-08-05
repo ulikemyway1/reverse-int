3430d47548d59a08a1283a417eeb1c91f03a943e
Alex_Nout <calium_by@mail.ru>
2023-08-05 13:45:22 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let reverseNum =''
+    if (number < 0) {
+        // reverseNum += '-';
+        number = number * (-1);
+    }
+while (number >=10) {
+    reverseNum += number%10;
+    number = (number - number%10)/10
+}
+reverseNum += number;
+return +reverseNum 
 }
